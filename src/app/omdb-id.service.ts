64842fd470a5f336e950dabb20d16f1f77d733e9
Elvis Morales <emorales@forumone.com>
2017-01-28 15:24:08 -0800

    Initial code concept to get a single movie by id.

--- src/app/omdb-id.service.ts
+++ src/app/omdb-id.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OmdbIdService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getMovieById(id: string) {
+    const url = 'http://www.omdbapi.com';
+    const params = new URLSearchParams();
+    // Movie ID
+    params.set('i', id);
+
+    return this.http.get(url, params)
+      .map(response => response.json().results);
+  }
 
 }
